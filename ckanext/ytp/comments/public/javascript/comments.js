05e4e0a6be602fb75f3d75cc1e780847c0853287
Wen Li <wen.li@ucl.ac.uk>
2015-04-28 15:11:11 +0100

    Improve the logic for showing editing forms
    Now the editing form will appear at proper place when edit/reply
    buttons are clicked. With/out the original comment according to
    the context.

--- ckanext/ytp/comments/public/javascript/comments.js
+++ ckanext/ytp/comments/public/javascript/comments.js
@@ -1,3 +1,13 @@
-function ShowCommentForm(id){
-    $("#" + id).removeClass('hidden');
+function ShowCommentForm(id, type){
+  // reset the status
+  $("form.ytp-form").addClass('hidden');
+  $("div.ytp-content").removeClass('hidden');
+
+  if(type==="edit"){
+    $("#" + "edit_" + id).removeClass('hidden');
+    $("#" + "content_" + id).addClass('hidden');
+  }
+  else{
+    $("#" + "reply_" + id).removeClass('hidden');
+  }
 }
